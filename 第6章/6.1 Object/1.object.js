2ada9e76ba546187fcc0f1b69b5eeaef8b28322d
WEID <chen@weid.ink>
2022-09-18 00:02:41 +0800

    feat: 对象字面量

---
+++ 第6章/6.1 Object/1.object.js
@@ -0,0 +1,15 @@
+// 显式创建
+let person = new Object()
+
+person.name = 'Kobe'
+person.age = 18
+
+console.log(person)
+
+// 对象字面量
+person = {
+    name: 'Answer',
+    age: 19
+}
+
+console.log(person)
