3264e314b5b59d5b1037ecc857f141d468172100
WEID <chen@weid.ink>
2022-09-19 23:41:35 +0800

    feat: 学习object相关代码

--- 第6章/6.1 Object/1.object.js
+++ 第6章/6.1 Object/1.object.js
@@ -13,3 +13,33 @@ person = {
 }
 
 console.log(person)
+
+person = {}
+person.name = 'KiKi'
+person.age = 29
+
+console.log(person)
+
+function displayInfo(args) {
+    let output = ""
+    if(typeof args.name === 'string') {
+        output += "Name: " + args.name + "\n"
+    }
+    if(typeof args.age === "number") {
+        output += "Age: " + args.age
+    }
+
+    console.log('displayInfo: ', output)
+}
+
+displayInfo(person)
+
+console.log(person['name'])
+console.log(person.name)
+
+let propertyName = 'name'
+console.log(person[propertyName])
+
+person['first name'] = 'Li'
+
+console.log(person['first name'])
